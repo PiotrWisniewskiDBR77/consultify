db074efbad5923cba40afd6636a280bc0a01626a
konradmilewski <konrad.milewski@appworks.pl>
2026-01-28 03:42:11 +0100

    fix: update AISchemaValidator.js re-export to use .ts extension

--- server/src/utils/AISchemaValidator.js
+++ server/src/utils/AISchemaValidator.js
@@ -1,2 +1,4 @@
-export * from './AISchemaValidator.js';
-export { default } from './AISchemaValidator.js';
+// Re-export from AISchemaValidator.ts for case-sensitive file systems
+// This file allows imports using lowercase 'aiSchemaValidator' to work on case-sensitive filesystems
+export * from './AISchemaValidator.ts';
+export { default } from './AISchemaValidator.ts';
