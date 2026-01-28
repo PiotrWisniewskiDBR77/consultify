ef6678fb693592ab1fe0055ab56990517ea7cc0d
konradmilewski <konrad.milewski@appworks.pl>
2026-01-28 03:48:38 +0100

    fix: remove default export from AISchemaValidator re-export files
    - AISchemaValidator.ts only has named exports, no default export
    - Removing 'export { default }' line that was causing runtime error
    - Fixes export error for aiSchemaValidator.js

--- server/src/utils/AISchemaValidator.js
+++ server/src/utils/AISchemaValidator.js
@@ -1,4 +1,3 @@
 // Re-export from AISchemaValidator.js (compiled) for case-sensitive file systems
 // This file allows imports using lowercase 'aiSchemaValidator' to work on case-sensitive filesystems
 export * from './AISchemaValidator.js';
-export { default } from './AISchemaValidator.js';
